36861471b31570b4a79f5fee94e86e1c1f45dd0e
uvadhar11 <111589719+uvadhar11@users.noreply.github.com>
2023-10-10 09:09:29 -0700

    halfway thru 10.10.23 and some stuff from before

--- src/js/controller.js
+++ src/js/controller.js
@@ -36,13 +36,20 @@ const renderSpinner = function (parentEl) {
 // npm init - makes our package.json file
 const showRecipe = async function () {
   try {
+    // getting hash from url - now we can load id based on the url
+    const id = window.location.hash.slice(1); // cut the hash (first character) from id
+    console.log(id);
+
+    // if no id, then return so there is no error - guard clause
+    if (!id) return;
+
     // 1) Loading recipe
     renderSpinner(recipeContainer); // calling the spinner function. This disappears when the content is loaded because this goes into the recipe container element, then when the content is loaded, that SAME container is emptied and the new code with the content is added to the same container, resulting in the spinner being gone.
 
     // fetch from api which returns a promise
     const res = await fetch(
       // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcc40'
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
     // convert to json
     const data = await res.json();
@@ -174,3 +181,27 @@ const showRecipe = async function () {
   }
 };
 showRecipe();
+
+// event listener for hash in url changing
+// window.addEventListener('hashchange', showRecipe);
+
+// need to also add this event listener for when the page is loaded, so that the recipe is shown when the page is loaded since if u paste the url in a new tab, it won't load the recipe.
+// window.addEventListener('load', showRecipe);
+
+// we have duplicated code so we can just put the events in an array and oop thru them since calling the same callback function
+['hashchange', 'load'].forEach(theEvent =>
+  window.addEventListener(theEvent, showRecipe)
+);
+
+///////////////////////////////////////
+// NOTES
+// MVC ARCHITECTURE
+// structure, maintainability, expandability is needed which is why we need a perfect architecture - we can make our own architecture (like with small projects), or for big projects - we can use a well established pattern like MVC, MVP, Flux, etc.
+// we can use a framework like React, Angular, Vue, etc. which has a built in architecture
+// components of any architecture: business logic (logic solving the problem/business does and needs like for banking its storing transactions), state (or application state - stores all data about application like page user using and stuff - single source of truth, ui is based on that - many state management libraries like redux or mobX), http library (make and recieve ajax requests like with fetch - most real world applications need this), application logic/router (implementation of application, handles navigation and UI events), persentation logic/ui layer (concerned with the visual part and displays the application state)
+// NEED a way to separate these components so we use MVC - model-view-controller architecture.
+// view - interacts with user/presentation logic
+// model - business logic, state, http library like data from web/backend
+// controller - application logic/router, connects view and model since view and model should exist completely independent from each other.
+// a goal of this model is to seperate business and presetnation logic so we need this controller to connect them both.
+// controller handles ui events and dispatches tasks to model and view. GETS UI EVENTS AS WELL.
